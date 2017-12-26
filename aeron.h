4759450c2e4653cea9998d8635fa3e9d0b2498dd
xujun <xujun@cloudtenfund.com>
2017-12-26 14:49:46 +0800

    change static to shared

--- aeron.h
+++ aeron.h
@@ -5,17 +5,21 @@
 extern "C" {
 #endif
 
-int aeron_initialize(char *aeron_dir);
-void aeron_destroy();
+#if defined(_MSC_VER)
+	#define AERON_CLIENT_CGO_EXPORT __declspec(dllexport)
+#endif 
 
-int aeron_add_publication(char *channel, int stream_id);
-void aeron_remove_publication(int publication_idx);
-int aeron_get_streamId(int publication_idx);
-int64_t aeron_publish(int publication_idx, char* msg, int msg_len);
-
-int aeron_add_subscription(char *channel, int stream_id);
 typedef int (*poll_handler_t)(char*, int);
-int aeron_poll(int subscription_idx, poll_handler_t handler);
+
+AERON_CLIENT_CGO_EXPORT int aeron_initialize(char *aeron_dir);
+AERON_CLIENT_CGO_EXPORT void aeron_destroy();
+
+AERON_CLIENT_CGO_EXPORT int aeron_add_publication(char *channel, int stream_id);
+AERON_CLIENT_CGO_EXPORT void aeron_remove_publication(int publication_idx);
+AERON_CLIENT_CGO_EXPORT int aeron_get_streamId(int publication_idx);
+AERON_CLIENT_CGO_EXPORT int64_t aeron_publish(int publication_idx, char* msg, int msg_len);
+AERON_CLIENT_CGO_EXPORT int aeron_add_subscription(char *channel, int stream_id);
+AERON_CLIENT_CGO_EXPORT int aeron_poll(int subscription_idx, poll_handler_t handler);
 
 #ifdef __cplusplus
 }
